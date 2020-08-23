bff440916c25200706aa7b970329f07fbd760ec5
jennymirei <68317136+jennymirei@users.noreply.github.com>
2020-08-23 19:18:52 +0900

    UI(GameManager)랑 SoundManager추가
    SceneChange Script
    SoundManager Script 추가
    Sound Source 추가
    MYS_WorkScene에 SoundManager GameObject 추가 및 SoundManger Script 적용
    UI Scene변경 적용 후처리 필요(게임 오버, 게임 클리어)
    씬 전환 시 Sound 작동 되는지 확인 필요

---
+++ TheLab_VR_Project/Assets/ES/Scripts/SceneChange.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public string SceneToLoad;
+
+    public GameObject MainUI;
+    public GameObject GameOverUI;
+    public GameObject GameClearUI;
+
+    public SoundManager SoundMngr;
+
+    AudioSource myAudio;
+    void Start()
+    {
+        MainUI.SetActive(true);
+        GameOverUI.SetActive(false);
+        GameClearUI.SetActive(false);
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("MYS_WorkScene");
+        myAudio = SoundMngr.GetComponent<AudioSource>();
+        myAudio.Play();
+    }
+
+    void Update()
+    {
+
+        //1. 게임이 오버되면 게임오버 UI로 전환이 필요
+        //   게임오버 UI만 true, 나머지는 false
+        //   Sound Play
+        myAudio = SoundMngr.GetComponent<AudioSource>();
+
+        // 2. 게임 클리어 되면 게임 클리어 UI로 전환이 필요
+        //    게임클리어 UI만 true, 나머지는 false
+        //   Sound Play
+    }
+
+
+}
---
+++ TheLab_VR_Project/Assets/ES/Scripts/SoundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    //음악플레이어
+    public AudioSource myAudio;
+
+    public AudioClip mainAudio;
+    public AudioClip puzzleSuccess;
+    public AudioClip puzzleFail;
+    public AudioClip gameStart;
+    public AudioClip gameOver;
+    public AudioClip gameClear;
+
+
+
+
+
+    void Awake()
+    {
+        myAudio = gameObject.GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        myAudio.Play();
+      
+    }
+
+
+}
